02ff54a6290ac60aad8fde37d0f55c7623323259
eon <dobrin_k1@abv.bg>
2023-12-23 19:57:19 +0200

    Hunger/Thirst Loss

--- plugins/needs/sh_plugin.lua
+++ plugins/needs/sh_plugin.lua
@@ -19,3 +19,43 @@ ix.char.RegisterVar("thirst", {
     isLocal = false,
     bNoDisplay = true,
 })
+
+ix.config.Add("hungerRate", 120, "How fast the player gets hungry.", function(newVal)
+    if ( SERVER ) then
+        for k, v in pairs(player.GetAll()) do
+            if not ( IsValid(v) ) then
+                continue
+            end
+        
+            if not ( v:GetCharacter() ) then
+                return
+            end
+
+            timer.Adjust("ix.Characters.Needs.Hunger." .. v:GetCharacter():GetID(), newVal)
+        end
+    end
+end, {
+    data = {min = 0, max = 100},
+    category = "Needs"
+})
+
+ix.config.Add("thirstRate", 120, "How fast the player gets thirsty.", function(newVal)
+    if ( SERVER ) then
+        for k, v in pairs(player.GetAll()) do
+            if not ( IsValid(v) ) then
+                continue
+            end
+        
+            if not ( v:GetCharacter() ) then
+                return
+            end
+
+            timer.Adjust("ix.Characters.Needs.Thirst." .. v:GetCharacter():GetID(), newVal)
+        end
+    end
+end, {
+    data = {min = 0, max = 100},
+    category = "Needs"
+})
+
+ix.util.Include("sv_hooks.lua")
--- plugins/needs/sv_hooks.lua
+++ plugins/needs/sv_hooks.lua
@@ -9,3 +9,43 @@ function PLUGIN:DoPlayerDeath(ply, attacker, dmgInfo)
 
     char:SetData("isConsuming", false)
 end
+
+function PLUGIN:PlayerLoadedCharacter(ply, newChar, oldChar)
+    if ( ply:GetCharacter() ) then
+        ply:GetCharacter():SetData("isConsuming", false)
+
+        if not ( timer.Exists("ix.Characters.Needs.Hunger." .. newChar:GetID()) ) then
+            timer.Create("ix.Characters.Needs.Hunger." .. newChar:GetID(), ix.config.Get("hungerRate", 60), 0, function()
+                if ( ply:GetCharacter() ) then
+                    local char = ply:GetCharacter()
+                    local hunger = char:GetHunger()
+
+                    if ( hunger > 10 ) then
+                        char:SetHunger(hunger - 1)
+                    end
+                end
+            end)
+        end
+
+        if not ( timer.Exists("ix.Characters.Needs.Thirst." .. oldChar:GetID()) ) then
+            timer.Create("ix.Characters.Needs.Thirst." .. oldChar:GetID(), ix.config.Get("thirstRate", 60), 0, function()
+                if ( ply:GetCharacter() ) then
+                    local char = ply:GetCharacter()
+                    local thirst = char:GetThirst()
+                    
+                    if ( thirst > 10 ) then
+                        char:SetThirst(thirst - 1)
+                    end
+                end
+            end)
+        end
+
+        if ( timer.Exists("ix.Characters.Needs.Hunger" .. oldChar:GetID()) ) then
+            timer.Remove("ix.Characters.Needs.Hunger" .. oldChar:GetID())
+        end
+
+        if ( timer.Exists("ix.Characters.Needs.Thirst" .. oldChar:GetID()) ) then
+            timer.Remove("ix.Characters.Needs.Thirst" .. oldChar:GetID())
+        end
+    end
+end
