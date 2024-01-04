9c3d372205968004d7c9cf008766df9bafea7036
eon <dobrin_k1@abv.bg>
2024-01-04 19:36:18 +0200

    Remove Stacks

--- plugins/stacks/sh_plugin.lua
+++
@@ -1,8 +0,0 @@
-
-PLUGIN.name = "Stackable Items"
-PLUGIN.description = "Adds the ability to stack items in one"
-PLUGIN.author = "Bilwin"
-PLUGIN.schema = "Any"
-PLUGIN.version = 1.0
-
-ix.util.Include("sh_meta.lua")
