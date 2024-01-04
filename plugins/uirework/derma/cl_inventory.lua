a6e83c64182510ad069581e10642b460dc621eb4
eon <dobrin_k1@abv.bg>
2024-01-04 20:02:20 +0200

    Update cl_inventory.lua

--- plugins/uirework/derma/cl_inventory.lua
+++ plugins/uirework/derma/cl_inventory.lua
@@ -280,7 +280,7 @@ AccessorFunc(PANEL, "iconSize", "IconSize", FORCE_NUMBER)
 AccessorFunc(PANEL, "bHighlighted", "Highlighted", FORCE_BOOL)
 
 function PANEL:Init()
-	self:SetIconSize(100)
+	self:SetIconSize(40)
 	self:ShowCloseButton(false)
 	self:SetDraggable(true)
 	self:SetSizable(true)
