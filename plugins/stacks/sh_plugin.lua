6a9cd088f94885f7653a806538d9aa830a7c94b2
eon <dobrin_k1@abv.bg>
2024-01-07 20:55:59 +0200

    Updates

---
+++ plugins/stacks/sh_plugin.lua
@@ -0,0 +1,11 @@
+local PLUGIN = PLUGIN
+
+// Credits: https://github.com/Bilwin/helix-plugins/tree/main/stacks
+
+PLUGIN.name = "Stackable Items"
+PLUGIN.description = "Adds the ability to stack items in one"
+PLUGIN.author = "Bilwin"
+PLUGIN.schema = "Any"
+PLUGIN.version = 1.0
+
+ix.util.Include("sh_meta.lua")
