ad6347662c0e299926a8b645a3c2ac1f2351ccc9
eon <dobrin_k1@abv.bg>
2024-01-08 02:35:10 +0200

    Update sh_citizen_electrician.lua

--- schema/classes/sh_citizen_electrician.lua
+++ schema/classes/sh_citizen_electrician.lua
@@ -3,4 +3,18 @@ CLASS.faction = FACTION_CITIZEN
 CLASS.isDefault = true
 CLASS.abbreviation = "Electrician"
 
+function CLASS:OnSet(ply)
+    local char = ply:GetCharacter()
+
+    if not ( char ) then
+        return
+    end
+
+    local inv = char:GetInventory()
+
+    if not ( inv:HasItem("repair_tool" ) ) then
+        inv:Add("repair_tool")
+    end
+end
+
 CLASS_CITIZEN_ELECTRICIAN = CLASS.index
