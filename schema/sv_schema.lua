3210519d0babaa9e40b4129a64ef79c8d687defa
eon <dobrin_k1@abv.bg>
2024-01-09 19:45:34 +0200

    Update sv_schema.lua

--- schema/sv_schema.lua
+++ schema/sv_schema.lua
@@ -5,6 +5,23 @@ function Schema:SlapPlayer(client)
 	end
 end
 
+// Credits: https://github.com/NebulousCloud/helix-hl2rp/blob/master/schema/sv_schema.lua
+function Schema:SearchPlayer(client, target)
+	if (!target:GetCharacter() or !target:GetCharacter():GetInventory()) then
+		return false
+	end
+
+	local name = hook.Run("GetDisplayedName", target) or target:Name()
+	local inventory = target:GetCharacter():GetInventory()
+
+	ix.storage.Open(client, inventory, {
+		entity = target,
+		name = name
+	})
+
+	return true
+end
+
 function Schema:SetCharBodygroup(ply, index, value)
 	if not ( IsValid(ply) ) then
 		return
