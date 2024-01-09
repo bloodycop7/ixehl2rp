f216d63d7b69648cfdb51ba1067c979004b0ae33
eon <dobrin_k1@abv.bg>
2024-01-09 22:15:05 +0200

    Updates

--- plugins/cmb/sv_hooks.lua
+++ plugins/cmb/sv_hooks.lua
@@ -1,66 +1,70 @@
 local PLUGIN = PLUGIN
 
 function PLUGIN:DoPlayerDeath(ply, attacker, dmgInfo)
+    if not ( IsValid(ply) ) then
+        return
+    end
+
     local char = ply:GetCharacter()
 
     if not ( char ) then
         return
     end
 
-    local numbers = {}
-
-    for k, v in pairs(string.ToTable(char:GetName())) do
-        if not ( isnumber(tonumber(v)) ) then
-            continue
-        end
-
-        if ( ix.cmbSystems.dispatchNumbers[tonumber(v)] ) then
-            numbers[#numbers + 1] = ix.cmbSystems.dispatchNumbers[tonumber(v)]
-        end
-    end
-
-    local tagline = "union"
-
-    for k, v in pairs(ix.cmbSystems.dispatchTaglines) do
-        if ( string.find(string.lower(char:GetName()), k) ) then
-            tagline = k
-        end
-    end
-
-    for k, v in ipairs(player.GetAll()) do
-        if not ( IsValid(v) ) then
-            continue
-        end
-
-        local char = v:GetCharacter()
-
-        if not ( char ) then
-            continue
-        end
-
-        if not ( Schema:IsCombine(v) ) then
-            continue
-        end
-
-        local sounds = {
-            "npc/overwatch/radiovoice/on3.wav",
-            "npc/overwatch/radiovoice/attention.wav",
-            "npc/overwatch/radiovoice/_comma.wav",
-            "npc/overwatch/radiovoice/lostbiosignalforunit.wav"
-        }
-
-        sounds[#sounds + 1] = "npc/overwatch/radiovoice/" .. tagline .. ".wav"
-
-        for k2, v2 in ipairs(numbers) do
-            sounds[#sounds + 1] = v2
-        end
-
-        sounds[#sounds + 1] = "npc/combine_soldier/vo/off1.wav"
-
-        ix.util.EmitQueuedSounds(ply, sounds, 0, 0.1, 35, 90)
-    end
-
     if ( Schema:IsCombine(ply) ) then
+        local numbers = {}
+
+        for k, v in pairs(string.ToTable(char:GetName())) do
+            if not ( isnumber(tonumber(v)) ) then
+                continue
+            end
+
+            if ( ix.cmbSystems.dispatchNumbers[tonumber(v)] ) then
+                numbers[#numbers + 1] = ix.cmbSystems.dispatchNumbers[tonumber(v)]
+            end
+        end
+
+        local tagline = "union"
+
+        for k, v in pairs(ix.cmbSystems.dispatchTaglines) do
+            if ( string.find(string.lower(char:GetName()), k) ) then
+                tagline = k
+            end
+        end
+
+        for k, v in ipairs(player.GetAll()) do
+            if not ( IsValid(v) ) then
+                continue
+            end
+
+            local char = v:GetCharacter()
+
+            if not ( char ) then
+                continue
+            end
+
+            if not ( Schema:IsCombine(v) ) then
+                continue
+            end
+
+            local sounds = {
+                "npc/overwatch/radiovoice/on3.wav",
+                "npc/overwatch/radiovoice/attention.wav",
+                "npc/overwatch/radiovoice/_comma.wav",
+                "npc/overwatch/radiovoice/lostbiosignalforunit.wav"
+            }
+
+            sounds[#sounds + 1] = "npc/overwatch/radiovoice/" .. tagline .. ".wav"
+
+            for k2, v2 in ipairs(numbers) do
+                sounds[#sounds + 1] = v2
+            end
+
+            sounds[#sounds + 1] = "npc/combine_soldier/vo/off1.wav"
+
+            ix.util.EmitQueuedSounds(v, sounds, 0, 0.1, 40, 90)
+        end
+
         ix.cmbSystems:MakeWaypoint({
             pos = ply:GetPos(),
             text = "BSL " .. char:GetName() .. ".",
--- schema/ranks/sh_cp_recruit.lua
+++ schema/ranks/sh_cp_recruit.lua
@@ -1,6 +1,7 @@
 RANK.name = "Recruit"
 RANK.faction = FACTION_CP
 RANK.description = "A recruit of the Civil Protection."
+RANK.skin = 1
 RANK.isDefault = true
 
 RANK_CP_RECRUIT = RANK.index
