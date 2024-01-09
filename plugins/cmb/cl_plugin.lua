d85c0f4dc1cc5e1753fddba034f71e8b65d51ae2
eon <dobrin_k1@abv.bg>
2024-01-09 14:42:10 +0200

    Update cl_plugin.lua

--- plugins/cmb/cl_plugin.lua
+++ plugins/cmb/cl_plugin.lua
@@ -53,35 +53,37 @@ function ix.cmbSystems:MakeWaypoint(data)
     ix.cmbSystems.waypoints[#ix.cmbSystems.waypoints + 1] = data
 end
 
-for i = 6, 40, 2 do
-    local value = Schema:ZeroNumber(i, 2)
+function PLUGIN:LoadFonts(font, genericFont)
+    for i = 6, 40, 2 do
+        local value = Schema:ZeroNumber(i, 2)
 
-    surface.CreateFont("ixCombineFont" .. value, {
-        font = "Frak",
-        size = ScreenScale(i),
-        weight = 100,
-        antialias = true,
-        extended = true,
-        shadows = true,
-    })
-    
-    // Registers fonts:
-        // "ixCombineFont06"
-        // "ixCombineFont08"
-        // "ixCombineFont10"
-        // "ixCombineFont12"
-        // "ixCombineFont14"
-        // "ixCombineFont16"
-        // "ixCombineFont18"
-        // "ixCombineFont20"
-        // "ixCombineFont22"
-        // "ixCombineFont24"
-        // "ixCombineFont26"
-        // "ixCombineFont28"
-        // "ixCombineFont30"
-        // "ixCombineFont32"
-        // "ixCombineFont34"
-        // "ixCombineFont36"
-        // "ixCombineFont38"
-        // "ixCombineFont40"
+        surface.CreateFont("ixCombineFont" .. value, {
+            font = "Frak",
+            size = ScreenScale(i),
+            weight = 100,
+            antialias = true,
+            extended = true,
+            shadows = true,
+        })
+        
+        // Registers fonts:
+            // "ixCombineFont06"
+            // "ixCombineFont08"
+            // "ixCombineFont10"
+            // "ixCombineFont12"
+            // "ixCombineFont14"
+            // "ixCombineFont16"
+            // "ixCombineFont18"
+            // "ixCombineFont20"
+            // "ixCombineFont22"
+            // "ixCombineFont24"
+            // "ixCombineFont26"
+            // "ixCombineFont28"
+            // "ixCombineFont30"
+            // "ixCombineFont32"
+            // "ixCombineFont34"
+            // "ixCombineFont36"
+            // "ixCombineFont38"
+            // "ixCombineFont40"
+    end
 end
