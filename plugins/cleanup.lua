711c8f3ac3a71b6a8eaf10d8447da9ef22ed26f1
eon <dobrin_k1@abv.bg>
2024-01-11 17:07:15 +0200

    Licenses Update

--- plugins/cleanup.lua
+++ plugins/cleanup.lua
@@ -4,7 +4,7 @@ PLUGIN.name = "Cleanup"
 PLUGIN.author = "eon"
 PLUGIN.description = "Adds a few configurations regarding cleanups."
 PLUGIN.license = [[
-Copyright 2024 bloodycop
+Copyright 2024 eon (bloodycop)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
