9061635deaf9af6619e3b9fb713d8ad621560d40
eon <dobrin_k1@abv.bg>
2024-02-04 17:35:54 +0200

    Update cleanup.lua

--- plugins/cleanup.lua
+++ plugins/cleanup.lua
@@ -1,53 +1,40 @@
 local PLUGIN = PLUGIN
 
-PLUGIN.name = "Cleanup"
-PLUGIN.author = "eon"
-PLUGIN.description = "Adds a few configurations regarding cleanups."
-PLUGIN.license = [[
-Copyright 2024 eon (bloodycop)
+PLUGIN.name = "Ragdoll Remover"
+PLUGIN.author = "eon (bloodycop)"
+PLUGIN.description = "Removes all ragdolls found on the ground."
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-]]
-
-ix.config.Add("shouldCleanupRagdolls", true, "Whether or not ragdolls should be cleaned up.", nil, {
-    category = "Cleanup"
-})
-
-ix.config.Add("shouldCleanupItems", true, "Whether or not items should be cleaned up.", nil, {
-    category = "Cleanup"
-})
-
-ix.config.Add("cleanupRate", 60, "How often the cleanup should run in seconds.", function(oldV, newV)
-    timer.Adjust("ix.Cleanup", newV)
+ix.config.Add("ragdollRemoveTime", 60, "How long to wait before removing ragdolls.", function(iOld, iNew)
+   if ( timer.Exists("ix.RagdollRemoveTimer") ) then
+      timer.Adjust("ix.RagdollRemoveTimer", iNew, 0)
+   end
 end, {
-    category = "Cleanup",
-    data = {min = 1, max = 3600},
+   data = {min = 0, max = 500},
+   category = PLUGIN.name
 })
 
-timer.Create("ix.Cleanup", 60, 0, function()
-    if ( ix.config.Get("shouldCleanupRagdolls", true) ) then
-        for _, v in ipairs(ents.FindByClass("prop_ragdoll")) do
-            if ( SERVER ) then
-                v:Remove()
-            end
-        end
+ix.config.Add("ragdollRemoveEnabled", false, "Should ragdoll remover be enabled?", function(bOld, bNew)
+   if ( bNew ) then
+      if ( timer.Exists("ix.RagdollRemoveTimer") ) then
+         return
+      end
 
-        for _, v in ipairs(ents.FindByClass("class C_ClientRagdoll")) do
-            if ( CLIENT ) then
-                v:Remove()
-            end
-        end
-    end
+      timer.Create("ix.RagdollRemoveTimer", ix.config.Get("ragdollRemoveTime", 60), 0, function()
+         for k, v in ipairs(ents.FindByClass("prop_ragdoll")) do
+            v:Remove()
+         end
 
-    if ( ix.config.Get("shouldCleanupItems", true) ) then
-        for _, v in ipairs(ents.FindByClass("ix_item")) do
-            if ( SERVER ) then
-                v:Remove()
+         if ( CLIENT ) then
+            for k, v in ipairs(ents.FindByClass("class C_ClientRagdoll")) do  
+               v:Remove()
             end
-        end
-    end
-end)
+         end
+      end)
+   else
+      if ( timer.Exists("ix.RagdollRemoveTimer") ) then
+         timer.Remove("ix.RagdollRemoveTimer")
+      end
+   end
+end, {
+   category = PLUGIN.name
+})
