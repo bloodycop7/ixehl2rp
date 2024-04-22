7c4d6c856a6fc928cd71cccba3144b825af1c68a
eon <dobrin_k1@abv.bg>
2024-04-23 00:17:44 +0300

    Improving code lol

--- plugins/voicemodes/sh_plugin.lua
+++ plugins/voicemodes/sh_plugin.lua
@@ -12,22 +12,22 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ]]
 
-ix.voiceModes = ix.voiceModes or { Stored = {
+PLUGIN.stored = {
     {
         name = "Normal",
     },
     {
-        name = "Yell",
+        name = "Whisper",
     },
     {
-        name = "Whisper",
+        name = "Yell",
     }
-} }
+}
 
-ix.voiceModes.ChatTypes = {
-    [1] = "ic",
-    [2] = "y",
-    [3] = "w"
+PLUGIN.chatTypes = {
+    "ic",
+    "w",
+    "y"
 }
 
 ix.command.Add("SetVoiceMode", {
@@ -44,7 +44,7 @@ ix.command.Add("SetVoiceMode", {
             return
         end
 
-        local voiceModeTable = ix.voiceModes.Stored[voiceMode]
+        local voiceModeTable = PLUGIN.stored[voiceMode]
 
         if not ( voiceModeTable ) then
             return
@@ -73,7 +73,7 @@ if ( SERVER ) then
             return
         end
 
-        local convertToChatType = ix.voiceModes.ChatTypes[listenerChar:GetData("voiceMode", 1)]
+        local convertToChatType = self.chatTypes[listenerChar:GetData("voiceMode", 1)]
 
         if not ( convertToChatType ) then
             return
